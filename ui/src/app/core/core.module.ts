7dfa5716ab42df004b649b5f7e0e0b9c19495197
eliobneto <eliobzgt@gmail.com>
2018-09-20 11:04:54 -0300

    adicionado core

---
+++ ui/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {EstoqueModule} from '../estoque/estoque.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    EstoqueModule
+  ],
+  declarations: []
+})
+export class CoreModule { }
