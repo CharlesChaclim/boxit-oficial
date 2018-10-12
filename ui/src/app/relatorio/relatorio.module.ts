a20a5dea3dabab11ecf1f5396a205d4279767e3b
Elio Bolognese Neto <eliobzgt@gmail.com>
2018-10-12 15:14:31 -0300

    Front-end - Gerar Relatório 2

--- ui/src/app/relatorio/relatorio.module.ts
+++ ui/src/app/relatorio/relatorio.module.ts
@@ -2,11 +2,21 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GerarComponent} from './gerar/gerar.component';
 import {RelatorioRoute} from './relatorio.routing';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {SweetAlert2Module} from '@toverux/ngx-sweetalert2';
 
 @NgModule({
   imports: [
     CommonModule,
-    RelatorioRoute
+    RouterModule,
+    RelatorioRoute,
+    FormsModule,
+    SharedModule,
+    NgbModule,
+    SweetAlert2Module
   ],
   declarations: [
     GerarComponent
