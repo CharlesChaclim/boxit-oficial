b0f4d0bedfa80bded507d9d0370e7a14bd0d16bd
Elio Bolognese Neto <eliobzgt@gmail.com>
2018-11-06 15:38:43 -0200

    Front-end Gerar Relatório 3

--- ui/src/app/relatorio/gerar/gerar.component.ts
+++ ui/src/app/relatorio/gerar/gerar.component.ts
@@ -5,17 +5,28 @@ import {Relatorio} from '../../core/model';
 import {RelatorioService} from '../relatorio.service';
 import {Router} from '@angular/router';
 import {ToastrService} from 'ngx-toastr';
+import {NgbDateCustomParserFormatter} from '../../shared/dateformat';
+import {NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-gerar',
   templateUrl: './gerar.component.html',
-  styleUrls: ['./gerar.component.scss']
+  styleUrls: ['./gerar.component.scss'],
+  providers: [
+    {provide: NgbDateParserFormatter, useClass: NgbDateCustomParserFormatter}
+  ]
 })
 export class GerarComponent implements OnInit {
   r = new Relatorio();
   estoque: any;
   cliente: any;
+  dataI: any;
+  dataF: any;
+  inicio = false;
+  dataIclick = false;
+  dataFclick = false;
   data_valida = false;
+  type: number;
 
   constructor(
     private relatorioService: RelatorioService,
@@ -30,8 +41,28 @@ export class GerarComponent implements OnInit {
     this.getCliente();
   }
 
+  dataPreenchidaInicio() {
+    this.dataIclick = true;
+    this.dataInicioMenorFim();
+  }
+
+  dataPreenchidaFim() {
+    this.dataFclick = true;
+    this.dataInicioMenorFim();
+  }
+
   dataInicioMenorFim() {
-    this.data_valida = this.r.dataInicio > this.r.dataFim;
+    this.data_valida = false;
+    if (this.dataIclick && this.dataFclick) {
+      this.inicio = true;
+      const dataIni = new Date(this.dataI.year, this.dataI.month - 1, this.dataI.day);
+      const dataFin = new Date(this.dataF.year, this.dataF.month - 1, this.dataF.day);
+      const diff = dataFin.getTime() - dataIni.getTime();
+      const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+      if (diffDays >= 0) {
+        this.data_valida = true;
+      }
+    }
   }
 
   getEstoque() {
