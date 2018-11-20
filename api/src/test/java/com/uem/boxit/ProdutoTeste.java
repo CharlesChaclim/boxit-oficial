ae8555770596b8786607886f25a0a8f140c29057
eliobneto <eliobzgt@gmail.com>
2018-11-20 13:10:28 -0200

    Teste 2

--- api/src/test/java/com/uem/boxit/ProdutoTeste.java
+++ api/src/test/java/com/uem/boxit/ProdutoTeste.java
@@ -1,24 +1,13 @@
 package com.uem.boxit;
 
-import com.amazonaws.services.dynamodbv2.xspec.S;
-import com.uem.boxit.dto.NewClienteDTO;
 import com.uem.boxit.dto.NewProdutoDTO;
-import com.uem.boxit.dto.UpdateClienteDTO;
-import com.uem.boxit.dto.UpdateQtdDTO;
 import com.uem.boxit.model.*;
-import com.uem.boxit.service.ClienteService;
-import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import com.uem.boxit.service.ProdutoService;
-
-import javax.transaction.Transactional;
-import java.util.*;
-import java.util.function.Function;
 
 public class ProdutoTeste extends BoxitApplicationTests{
 
@@ -38,5 +27,76 @@ public class ProdutoTeste extends BoxitApplicationTests{
         Assertions.assertThat(produtoService.getOne(id).get().getNome()).isEqualTo(nome);
     }
 
+    @Test
+    public void salvar() {
+        NewProdutoDTO produto = new NewProdutoDTO();
+        String nome = "Memória RAM Kingstom DDR3";
+        Double preco = 400000.0;
+        Double desconto = 0.0;
+        Integer qtd = 0;
+        Integer lote = 10;
+        String sku = "RAM-0004";
+        Integer categoriaId = 1;
+        String descricao = "Memória antiga";
+        produto.setNome(nome);
+        produto.setPreco(preco);
+        produto.setDesconto(desconto);
+        produto.setQtd(qtd);
+        produto.setUnidadeLote(lote);
+        produto.setSku(sku);
+        produto.setCategoriaId(categoriaId);
+        produto.setDescricao(descricao);
+        Produto produtoTeste = produtoService.salvar(produto);
+        Assertions.assertThat(produtoTeste.getNome()).isEqualTo(nome);
+        Assertions.assertThat(produtoTeste.getPreco()).isEqualTo(preco);
+        Assertions.assertThat(produtoTeste.getDesconto()).isEqualTo(desconto);
+        Assertions.assertThat(produtoTeste.getQtd()).isEqualTo(qtd);
+        Assertions.assertThat(produtoTeste.getUnidadeLote()).isEqualTo(lote);
+        Assertions.assertThat(produtoTeste.getSku()).isEqualTo(sku);
+        Assertions.assertThat(produtoTeste.getCategoria().getId()).isEqualTo(categoriaId);
+        Assertions.assertThat(produtoTeste.getDescricao()).isEqualTo(descricao);
+    }
 
+    @Test
+    public void nomeExist() {
+        String nome = "Memória RAM Kingstom DDR3";
+        Assertions.assertThat(produtoService.nomeExist(nome)).isEqualTo(true);
+    }
+
+    @Test
+    public void skuExist() {
+        String sku = "PRS-7400";
+        Assertions.assertThat(produtoService.skuExist(sku)).isEqualTo(true);
+    }
+
+    @Test
+    public void atualizar() {
+        Produto produto = new Produto();
+        int id = 1;
+        String nome = "Processador Intel Core i7-8700k";
+        Double preco = 800000.0;
+        Double desconto = 0.0;
+        Integer lote = 5;
+        String descricao = "Processador muito bom";
+        produto.setNome(nome);
+        produto.setPreco(preco);
+        produto.setDesconto(desconto);
+        produto.setUnidadeLote(lote);
+        produto.setDescricao(descricao);
+        Produto produtoTeste = produtoService.atualizar(id, produto);
+        Assertions.assertThat(produtoTeste.getNome()).isEqualTo(nome);
+        Assertions.assertThat(produtoTeste.getPreco()).isEqualTo(preco);
+        Assertions.assertThat(produtoTeste.getDesconto()).isEqualTo(desconto);
+        Assertions.assertThat(produtoTeste.getUnidadeLote()).isEqualTo(lote);
+        Assertions.assertThat(produtoTeste.getDescricao()).isEqualTo(descricao);
+    }
+
+    @Test
+    public void filtrar() {
+        String nome = "Memória RAM Kingstom DDR3";
+        String categoria = "Memória RAM";
+        Pageable p = new PageRequest(0,20);
+        Assertions.assertThat(produtoService.filtrar(nome, categoria,2, p).getContent().get(0).getNome()).isEqualTo(nome);
+        Assertions.assertThat(produtoService.filtrar(nome, categoria,0, p).getContent().get(0).getNome()).isEqualTo(nome);
+    }
 }
